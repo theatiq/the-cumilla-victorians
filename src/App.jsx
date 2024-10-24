25302740a23032d20ec0a5e851137590c0435c03
theatiq <theatiq@gmail.com>
2024-10-25 05:35:49 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Banner from "./components/Banner";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="w-11/12 mx-auto">
+      <Header></Header>
+      <Banner></Banner>
+    </div>
+  );
+}
+
+export default App;
